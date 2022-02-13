c1957da9b7e9fb4c70c96e7b22f1b8a9b2aad5ac
Sudhanshu Kumar <sudhansan5@gmail.com>
2022-02-13 19:11:35 +0530

    new springboot project added

---
+++ backend/src/test/java/com/sudhan/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudhan.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
