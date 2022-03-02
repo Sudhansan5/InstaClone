c9db1fced95e2772f9602dee3260216132017064
Sudhanshu Kumar <sudhansan5@gmail.com>
2022-03-02 20:13:53 +0530

    user controller added

--- backend/src/main/java/com/sudhan/backend/Controller/UserController.java
+++ backend/src/main/java/com/sudhan/backend/Controller/UserController.java
@@ -1,18 +1,22 @@
 package com.sudhan.backend.Controller;
 
 import com.sudhan.backend.Entity.Users;
+import com.sudhan.backend.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    UserService userService;
     @PostMapping("")
-    private boolean submitUser(@RequestBody Users users){
-        return true;
+    private Users submitUser(@RequestBody Users users){
+        return userService.submitMetaDataOfUser(users);
     }
     @GetMapping("/{userid}")
     private Users getUserDetails(@PathVariable("userid") String userId){
-        return new Users();
+        return userService.displayUserMetaData(userId);
     }
 }
