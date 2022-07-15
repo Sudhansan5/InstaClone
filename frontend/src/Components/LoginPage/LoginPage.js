915dec3c4fa36bd167df059b00ecbe2324bf0618
Sudhanshu Kumar <sudhansan5@gmail.com>
2022-07-15 15:58:58 +0530

    login component

---
+++ frontend/src/Components/LoginPage/LoginPage.js
@@ -0,0 +1,16 @@
+import React , {Component} from 'react';
+class LoginPage extends Component{
+    constructor(props){
+        super(props);
+        this.state={ }
+    }
+    render(){
+        return(
+        <div>
+            Hello World
+        </div>
+    );
+    }
+}
+
+export default LoginPage;
